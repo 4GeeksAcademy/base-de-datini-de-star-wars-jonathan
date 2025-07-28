f5e6f5b7172605e35ee7b584b93bfe692ea48771
Jonathan <jonyoner@gmail.com>
2025-07-28 10:45:02 +0000

    para los vagos

--- src/models.py
+++ src/models.py
@@ -24,7 +24,6 @@ class User(db.Model):
 
 class Favorite(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
     character_id: Mapped[int] = mapped_column(ForeignKey("character.id"))
@@ -37,7 +36,10 @@ class Favorite(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+            "vehicle_id": self.vehicle_id,
         }
 
 
